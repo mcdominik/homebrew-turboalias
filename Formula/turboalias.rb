25bccdf1709efa7e6c46384225d6164c2a36d5b4
Dominik M <dominik.mackiewicz@dtr.org>
2025-11-09 14:27:58 +0100

    update sha

--- Formula/turboalias.rb
+++ Formula/turboalias.rb
@@ -2,9 +2,9 @@ class Turboalias < Formula
   include Language::Python::Virtualenv
 
   desc "Fast shell alias management tool"
-  homepage "https://github.com/yourusername/turboalias"
+  homepage "https://github.com/mcdominik/turboalias"
   url "https://files.pythonhosted.org/packages/source/t/turboalias/turboalias-0.0.1.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "8b5a5dd251f96ab5b6dfc7930660bcdfa5ad0dd6a3efd0c6b29e61c2fe075db0"
   license "MIT"
 
   depends_on "python@3.12"
